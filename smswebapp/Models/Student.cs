76cc1aa81d6045690c0cb49e139a1546f6b3f74c
Manisha-dotcom-dot <manshusachdeva1@gmail.com>
2025-06-17 12:56:50 +0530

    Update Student.cs

--- smswebapp/Models/Student.cs
+++ smswebapp/Models/Student.cs
@@ -3,9 +3,4 @@ public class Student{
     public int Id {get; set;}
     public string Name {get; set;}
     public string Address {get; set;}
-
-    public Student(string name)
-        {
-            Name = name; // Initialize the property
-        }
 }
